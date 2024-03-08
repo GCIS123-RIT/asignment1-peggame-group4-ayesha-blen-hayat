63b5b9ba6b8d0caf162d99d1d2061710a4d4d380
him2079 <145419023+him2079@users.noreply.github.com>
2024-03-08 23:48:28 +0400

    Add files via upload

---
+++ Location.java
@@ -0,0 +1,20 @@
+public class Location {
+    private int row;
+    private int col;
+    
+    public Location(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+    
+    //Retrieves Row.
+    public int getRow() {
+        return row;
+    }
+    //Retrieves Column.
+    public int getCol() { 
+        return col;
+    }
+
+    
+}
